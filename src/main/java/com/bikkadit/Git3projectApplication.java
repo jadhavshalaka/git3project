97d68aa00a2e6edc44432cac9691103720c0401d
shalaka <shalakaj23@gmail.com>
2021-10-07 22:35:58 +0530

    ISE-2:git3 project created

---
+++ src/main/java/com/bikkadit/Git3projectApplication.java
@@ -0,0 +1,13 @@
+package com.bikkadit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git3projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Git3projectApplication.class, args);
+	}
+
+}
